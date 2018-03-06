2d00e2d3e33fbcd35ff07669697cc522e3291875
cyapa <kymcwyapa@gmail.com>
2018-03-06 21:02:54 +0200

    Currancy converter app created
    Currancy converter app created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,86 @@
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { EditorBorderStyle } from 'material-ui';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Paper from 'material-ui/Paper'
+import TextField from 'material-ui/TextField'
+import Checkbox from 'material-ui/Checkbox'
+import RaisedButton from 'material-ui/RaisedButton'
+injectTapEventPlugin();
 
+
+const RATE = 1.8;
+
+class App extends Component {
+	constructor() {	
+
+		super();
+
+		this.state = {
+			euros: 0,
+			dollars: 0,
+			doRound: false,
+			bgColor: 0,
+		}
+	}
+
+updateEuro = (input)=>{
+	let eur = parseFloat(input)
+	let dlr = this.state.doRound? Math.round(eur * RATE) : eur * RATE
+
+	this.setState(
+		{
+			euros:eur,
+			dollars:dlr,
+			doRound: this.state.doRound
+		}
+	);
+}
+
+updateDollar = (input) =>{
+	let dlr = parseFloat(input)
+	let eur = this.state.doRound? Math.round(dlr/RATE) : dlr/RATE;
+
+	this.setState({
+		euros:eur,
+		dollars:dlr,
+		doRound:this.state.doRound
+	});
+}
+
+roundAll = (e,isChecked)=>{
+	
+
+	this.setState({
+		euros: this.state.euros,
+		dollars:this.state.dollars,
+		doRound:isChecked
+	});
+	 
+}
+
+	
+  render() {
+	// building a css style color hex string, e.g., '#0000ff'
+	const c = this.state.bgColor;
+	const bgcol = "#"+(c*256*256+c*256+255).toString(16);
+	
+    return (
+		<MuiThemeProvider>
+			<Paper style={{width: 600,}}>
+				<div>
+					Euros: <TextField value={this.state.euros} onChange={(e,v)=>{this.updateEuro(v);}} />
+				</div><div>
+					Dollars: <TextField value={this.state.dollars} onChange={(e,v)=>{this.updateDollar(v)}}/>
+				</div><div>
+					<Checkbox label="Round" onCheck= {this.roundAll}  />
+				</div><div>
+					<RaisedButton label="Clear"/>
+				</div>
+			</Paper>
+		</MuiThemeProvider>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,109 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import  CurrencyConverter from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 
-// function formatDate(date) {
-//     return date.toLocaleDateString();
-//   }
-  
 
-// function Avatar(props){
-//     return(
-//         <img className="Avatar"
-//             src={props.user.avatarUrl}
-//             alt={props.user.name}
-//         />
-//     )
-// }
+    ReactDOM.render(<CurrencyConverter />, document.getElementById('root'));
 
-// function UserInfo(props){
-//     return(
-//         <div className="UserInfo">
-//         <Avatar user={props.author}/>
-//           <div className="UserInfo-name">
-//             {props.author.name}
-//           </div>
-//         </div>  
-//     )
-// }
-
-// function Comment(props) {
-//     return (
-//       <div className="Comment">
-//         <UserInfo author={props.author}/>
-//         <div className="Comment-text">
-//           {props.text}
-//         </div>
-//         <div className="Comment-date">
-//           {formatDate(props.date)}
-//         </div>
-//       </div>
-//     );
-//   }
-  
-//   const comment = {
-//     date: new Date(),
-//     text: 'I hope you enjoy learning React!',
-//     author: {
-//       name: 'Hello Kitty',
-//       avatarUrl: 'http://placekitten.com/g/64/64',
-//     },
-//   };
-  
-
-// ReactDOM.render( <Comment
-//     date={comment.date}
-//     text={comment.text}
-//     author={comment.author}
-//   />,document.getElementById('root'))
-
-
-class Clock extends React.Component{
-  
-  constructor(props){
-      super(props);
-      this.state = {
-          date: new Date()
-        }
-  }
-  
-  componentDidMount(){
-    this.timerID = setInterval(()=>this.tick(), 1000);
-  }
-
-  componentWillUnmount(){
-
-    clearInterval(this.timerID);
-
-  }
-
-  tick(){
-      this.setState(
-          {date: new Date()}
-      );
-  }
-    render(){
-   
-    return(
-        <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
-    );
-}
-}
-
-
-function tick() {
-    ReactDOM.render(<Clock/>, document.getElementById('root'));
-  }
-  
-  setInterval(tick, 1000);
-  
-  
 
 
 registerServiceWorker();
